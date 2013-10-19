ba223ab787b8661652506f53b7d985fe90da03c8
Eric Normand <ericwnormand@gmail.com>
2013-10-18 19:52:31 -0500

    Add docstrings.

--- src/playnice/core.clj
+++ src/playnice/core.clj
@@ -3,14 +3,25 @@
             [pantomime.mime :refer [mime-type-of]]
             [playnice.dispatch :as dispatch]))
 
-(defn wrap-fake-methods [h]
+(defn wrap-fake-methods
+  "Middleware to wrap a handler to use the HTTP request method
+  specified in the __method query or post parameter if it exists.
+
+  Note that this middleware must be called after the query and/or post
+  parameters are parsed using ring.middleware/wrap-params or similar."
+  [hdlr]
   (fn [req]
     (if-let [request-method (or (get-in req [:params :__method])
                                 (get-in req [:params "__method"]))]
-      (h (assoc req :request-method (keyword (string/lower-case request-method))))
-      (h req))))
+      (hdlr (assoc req :request-method (keyword (string/lower-case request-method))))
+      (hdlr req))))
 
-(defn wrap-ip-forwarding [hdlr]
+(defn wrap-ip-forwarding
+  "Middleware to wrap a handler to restore the :remote-addr value in
+  the Ring request based on the X-Forwarded-For header. It also adds
+  the X-Forwarded-By header, which will contain the IP address of the
+  nginx proxy which passed on the request, if there was one."
+  [hdlr]
   (fn [req]
     (if-let [ip (get-in req [:headers "x-forwarded-for"])]
       (hdlr (-> req
@@ -18,13 +29,16 @@
                 (assoc-in [:headers "x-forwarded-by"] (:remote-addr req))))
       (hdlr req))))
 
-(defn wrap-fake-accept [h]
+(defn wrap-fake-accept
+  "Middleware to wrap a handler to change the Accept header based on
+  the file extension in the URL path, if it exists."
+  [hdlr]
   (fn [req]
     (if-let [[_ pre-extension extension] (re-matches #"(.+)(\.[^./]+)" (:uri req))]
-      (h (-> req
+      (hdlr (-> req
              (assoc-in [:headers "accept"] (mime-type-of extension))
              (assoc :uri pre-extension)))
-      (h req))))
+      (hdlr req))))
 
 (defn split-path [p]
   (vec (drop 1 (string/split p #"/"))))
@@ -37,11 +51,20 @@
 (defn split-path-kws [p]
   (vec (map maybe-kw (split-path p))))
 
-(defn dispatch [dsp req]
+(defn dispatch
+  "Dispatch a request (req) to the appropriate handler in the routing
+  tree (dsp)."
+  [dsp req]
   (let [p (split-path (:uri req))]
     (dispatch/dispatch dsp (assoc req
                              :path-segments p
                              :remaining-path-segments p))))
 
-(defn dassoc [dsp path handler]
+(defn dassoc
+  "Associate a handler with a path in the routing tree (dsp).
+
+   Returns a new routing tree.
+
+   Will throw an exception when the path already exists in the tree."
+  [dsp path handler]
   (dispatch/dassoc dsp (split-path-kws path) handler))
